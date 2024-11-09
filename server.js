856166c83b3b0e5e6f06db5b08510296581c9ce0
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2024-11-09 07:30:03 +0100

    api service creation

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require ("express")
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.json({
+        status:"success",
+        message:"Api up and runnig "
+    })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log(`Server started on Port ${PORT}`);
+    
+})
