49475696bb30cf8a6a25cd847d7652b4110aefd6
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2025-02-12 15:47:23 +0100

    chore: cors handling

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const { initializeSavingsWorkers } = require("./jobs/savingsWorker");
 const app = express();
 
 app.use(cors())
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000", // Your frontend URL
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true, // Allow cookies & authentication headers
-//   })
-// )
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow cookies & authentication headers
+  })
+)
 app.use(express.json());
 app.use(helmet());
 app.use(rateLimiter);
